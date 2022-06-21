f349f00290c9727a173c35c931fb4b18df25d628
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2022-06-20 22:01:10 -0700

    update

--- towers/tower.py
+++ towers/tower.py
@@ -1,3 +1,6 @@
 class Tower:
-    def __init__(self):
-        pass
+    def __init__(self,length,height,xCoord,yCoord):
+        self.length = length
+        self.height = height
+        self.xCoord = xCoord
+        self.yCoord = yCoord
