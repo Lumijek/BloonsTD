98e424b4dc9aa1ccf87472c9bb79412bbf6486bb
Lumijek <lumijekpr@gmail.com>
2022-06-23 13:51:06 +0530

    Map fix

--- main.py
+++ main.py
@@ -17,7 +17,21 @@ font = pygame.font.SysFont("Arial" , 18 , bold = True)
 def load_map(map_name, width, height):
 	current_map = pygame.image.load(map_name)
 	current_map = pygame.transform.scale(current_map, (width, height))
+	current_map = pygame.transform.rotate(current_map, 0)
+
 	return current_map
+
+def display_map(screen, map_name, divider):
+	red_map = load_map(map_name, width/2 - 15, height)
+	blue_map = load_map(map_name, width/2 - 15, height)
+	divider = pygame.image.load(divider)
+
+	blue_map = pygame.transform.flip(blue_map, True, False)
+	divider = pygame.transform.scale(divider, (30, 600))
+
+	screen.blit(red_map, (0, 0))
+	screen.blit(blue_map, (width/2 + 15, 0))
+	screen.blit(divider, (width/2 - 15, 0))
 
 def update_fps():
 	fps = str(int(clock.get_fps()))
@@ -30,8 +44,7 @@ while True:
 			pygame.quit()
 			sys.exit()
 
-	c_map = load_map("images/maps/Monkey_meadow.png", width, height)
-	screen.blit(c_map, (0, 0))
+	display_map(screen, "images/maps/bloon_map_1.png", "images/utility/brick_divider.png")
 	screen.blit(update_fps(), (10,0))
 
 	pygame.display.update()
