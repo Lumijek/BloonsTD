7c28b19e62b434d0180f30bf6925949428992987
Lumijek <lumijekpr@gmail.com>
2022-06-24 22:05:14 +0530

    formatting issues

--- towers/tower.py
+++ towers/tower.py
@@ -1,6 +1,8 @@
 import math
+
+
 class Tower:
-    def __init__(self,length,height,xCoord,yCoord,rangeR,price,splash,damage):
+    def __init__(self, length, height, xCoord, yCoord, rangeR, price, splash, damage):
         self.length = length
         self.height = height
         self.xCoord = xCoord
@@ -9,27 +11,29 @@ class Tower:
         self.price = price
         self.splash = splash
         self.damage = damage
+
     def inRange(self, balloonX, balloonY):
-        xDiff = balloonX-self.xCoord
-        yDiff = balloonY-self.yCoord
-        if xDiff**2 + yDiff**2<=self.rangeR**2:
+        xDiff = balloonX - self.xCoord
+        yDiff = balloonY - self.yCoord
+        if xDiff**2 + yDiff**2 <= self.rangeR**2:
             return True
         else:
             return False
-    #Thinking about this still
-    '''def splashRange(self,balloonX, balloonY,splashR):
+
+    # Thinking about this still
+    """def splashRange(self,balloonX, balloonY,splashR):
         if self.splash:
             return True
         else:
-            return False'''
-    def shoot(self,balloon):
-        #if splash = true, will be called in here as well
+            return False"""
+
+    def shoot(self, balloon):
+        # if splash = true, will be called in here as well
         if self.inRange(balloon.getX(), balloon.getY()):
             balloon.setHealth(balloon.getHealth - self.damage)
-        
 
     def getX(self):
         return self.xCoord
+
     def getY(self):
         return self.yCoord
-
