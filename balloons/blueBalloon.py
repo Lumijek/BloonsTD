d8ae1a02fd98b7721d3e0be6cfbf2d777c87d672
Ameya <agandhi766@student.fuhsd.com>
2022-07-13 22:09:05 -0700

    balloons

--- balloons/blueBalloon.py
+++ balloons/blueBalloon.py
@@ -5,5 +5,7 @@ class BlueBalloon(Balloon):
     def __init__(self):
         super().__init__()
         self.img = pygame.image.load("images/balloon_images/blueballoon.png")
+        img_size = (23,29)
+        self.img = pygame.transform.scale(self.img, img_size)
         self.health = 2
         self.damage = 2
